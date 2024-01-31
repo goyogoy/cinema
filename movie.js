5f0f20351062fc358c753f271b2e802c507d6da8
Goyo <lui14150@gmail.com>
2024-01-31 15:02:02 +0100

    commit

--- movie.js
+++ movie.js
@@ -1,4 +1,4 @@
-const ssoTedbReadApiKey = "eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiI0MzIxNzJlYmU0YWJhODBmNGU2MzA1OGNlNGVjMWY1MSIsInN1YiI6IjY1YWZjMDdmYWFkOWMyMDEwOTA0NzQ1YiIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.-B-55Bp1Mswz0ScfLXmcoEHqIqXG-vSkdxSioG1_VTY"
+const ssoTedbReadApiKey = "eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiIzZjA3NGE3ZjBhMWZhOTllOThlMGVjZWJmODdkZDNmZCIsInN1YiI6IjY1YWZjMDdmYWFkOWMyMDEwOTA0NzQ1YiIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.hqpwUyMkWou5v8500grCh-nx1iLOtZpJlOFKCDK5Gq8"
 
 window.onload = async () =>{
     if(!location.search.includes("request_token=")){
@@ -22,29 +22,32 @@ window.onload = async () =>{
     }
 }
 
+async function getNewTMDBToken(){
+    const options = {
+        method: 'GET',
+        headers: {
+        accept: 'application/json',
+        Authorization: `Bearer ${ssoTedbReadApiKey}`
+        }
+    };
+    
+    let tokenRequest = await fetch('https://api.themoviedb.org/3/authentication/token/new', options).catch(err => console.error("error:" + err));
+    let tokenData = await tokenRequest.json()
+    console.log(tokenData)
+
+    return tokenData;
+}
+
 async function redirectUserToSSD(){
     let tokenData = await getNewTMDBToken()
     if (!tokenData.success){
         return alert("Une erreur est survenue et je peux pas vous identifier")
     }
     location.href = `https://www.themoviedb.org/authenticate/${tokenData.request_token}?redirect_to=http://127.0.0.1:5500`
+    console.log(tokenData)
 }
 
-const options = {
-    method: 'GET',
-    headers: {
-      accept: 'application/json',
-      Authorization: `Bearer ${ssoTedbReadApiKey}`
-    }
-  };
-  
-  let tokenRequest = await fetch('https://api.themoviedb.org/3/authentication', options).catch(err => console.error("error:" + err));
-  if (!tokenRequest){
-    return
-  }
-  let tokenData = await tokenRequest.json()
 
-  return tokenData;
 
 async function getNewSession(token) {
     const option ={
